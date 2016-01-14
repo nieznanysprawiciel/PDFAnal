562541ebbba67daa2d15e8dfd7f4d15d86c50762
Łukasz <luke.pochrzest@gmail.com>
2016-01-14 22:05:50 +0100

    Szybsze wczytywanie predefiniowanych kategorii

--- Classifier/src/PDFAnal/Category.cs
+++ Classifier/src/PDFAnal/Category.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using LAIR.ResourceAPIs.WordNet;
@@ -10,6 +11,9 @@ namespace PDFAnal
 {
     public class Category
     {
+        public static string STRING_REPRESENTATION_FIRST_LINE_KEY = "name";
+        public static string STRING_REPRESENTATION_SECOND_LINE_KEY = "wordcount";
+
         public string Name { get; private set; }
         public int WordCount { get; private set; }
         public Dictionary<SynSet, int> SynSetDictionary { get; private set; }
@@ -29,6 +33,89 @@ namespace PDFAnal
                     return nextPair.Value.CompareTo(firstPair.Value);
                 }
             );
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder(  STRING_REPRESENTATION_FIRST_LINE_KEY + "=" + Name + Environment.NewLine + 
+                                                        STRING_REPRESENTATION_SECOND_LINE_KEY + "=" + WordCount);
+	        foreach (var keyValuePair in SynSetOrderedList)
+	        {
+                builder.Append(Environment.NewLine + keyValuePair.Key.ID + "|" + keyValuePair.Value);
+	        }
+            return  builder.ToString();
+        }
+
+        public static Category FromString(WordNetEngine wordNetEngine, String text)
+        {
+            string categoryName;
+            int wordCount;
+            Dictionary<SynSet, int> synSetDictionary;
+
+            //  split into lines
+            var lines = Regex.Split(text, "\r\n|\r|\n");
+            
+            //  first line "name=.."
+            var firstLineSplit = lines[0].Split(new char[] {'='}, StringSplitOptions.RemoveEmptyEntries);
+            if (firstLineSplit.Length != 2)
+            {
+                Utility.Log("tu1");
+                return null;
+            }
+            if (firstLineSplit[0] != STRING_REPRESENTATION_FIRST_LINE_KEY)
+            {
+                Utility.Log("tu2");
+                return null;
+            }
+            categoryName = firstLineSplit[1];
+
+            //  second line "wordcount=.."
+            var secondLineSplit = lines[1].Split(new char[] {'='}, StringSplitOptions.RemoveEmptyEntries);
+            if (secondLineSplit.Length != 2)
+            {
+                Utility.Log("tu3");
+                return null;
+            }
+            if (secondLineSplit[0] != STRING_REPRESENTATION_SECOND_LINE_KEY || !Int32.TryParse( secondLineSplit[1], out wordCount))
+            {
+                Utility.Log("tu4");
+                return null;
+            }
+        
+            synSetDictionary = new Dictionary<SynSet,int>();
+
+            //  rest of the lines - dictionary
+            for(int i = 2 ; i < lines.Length ; ++i)
+            {
+                var lineSplit = lines[i].Split(new char[] {'|'}, StringSplitOptions.RemoveEmptyEntries);
+                if (lineSplit.Length != 2)
+                {
+                    break;
+                }
+
+                //  synset word count
+                int synSetWordCount;
+                if (!Int32.TryParse(lineSplit[1], out synSetWordCount))
+                {
+                    Utility.Log("tu6");
+                    return null;
+                }
+
+                //  synset
+                var synSet = wordNetEngine.GetSynSet(lineSplit[0]);
+                if (synSet == null)
+                {
+                    Utility.Log("tu7");
+                    return null;
+                }
+
+                synSetDictionary.Add(synSet, synSetWordCount);
+            }
+
+            Utility.Log("loaded " + categoryName + " (" + wordCount + ") with " + synSetDictionary.Count + " synsets");
+
+            return new Category(categoryName, wordCount, synSetDictionary);
         }
 
     }
